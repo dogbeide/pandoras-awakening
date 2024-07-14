2b3583f948e2dc678d8265668398519f39968977
David Ogbeide <boyowadavid@gmail.com>
2024-07-14 17:18:58 -0500

    implement FAQ click expand animation

---
+++ app/faq/FaqItem.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { useState } from "react";
+import { justAnotherHand } from '../lib/fonts'
+
+import './faq-item.css'
+import '@/app/components/buttons/btn-cta.css'
+
+export type FaqType = { 
+  q: string,
+  a: Array<string | Array<string>>,
+  s?: string,
+}
+
+export default function FaqItem({ key, faq } : {
+  key: number,
+  faq: FaqType
+}) {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setExpanded((prevState) => !prevState);
+  }
+
+  return (
+    <div
+      className="w-full px-3 flex flex-col items-center"
+    >
+      <div className={`flex flex-col items-center mb-5 max-md:mb-3 mx-3 w-[50%] max-sm:w-[100%] border-2 rounded-sm 
+                      bg-black bg-opacity-10 'border-pandoras-teal'`}
+      >
+        
+        <button onClick={toggleExpanded}
+          className={`${justAnotherHand.className} btn-cta-text 'hover:text-pandoras-teal' 
+                      font-bold px-2 py-2 max-md:py-1 text-3xl max-md:text-2xl max-sm:text-xl tracking-wider text-slate-200`}
+        >
+          {faq.q}
+        </button>
+
+        {<div 
+          className={`${expanded ? 'expanded' : ''} faq-answer text-sm text-slate-100 transition-all ease-in-out duration-200`}
+        >
+          {faq.a.map((a, idx) => {
+
+            return <span key={idx}>{
+              Array.isArray(a) ?
+                <ul className="list-disc ml-8">
+                  {a.map((li, idx) => (
+                    <li key={idx}>{li}</li>
+                  ))} 
+                </ul> :
+                a
+            }
+              <br/><br/>
+            </span>
+          })}
+        </div>}
+
+      </div>
+    </div>
+  )
+}
