3b4ee66aecfb2e3d267dccd49b1f907c07f4a6ec
David Ogbeide <boyowadavid@gmail.com>
2024-07-26 11:05:44 -0500

    convert footer year to dynamic copyright year

--- app/components/footer/SubFooter.jsx
+++ app/components/footer/SubFooter.jsx
@@ -2,7 +2,7 @@ export default function SubFooter() {
 
   return (
     <div className="bg-gradient-to-br from-white via-pandoras-teal to-white py-3 text-center text-sm max-sm:text-xs">
-      <span>© 2021 Pandora&apos;s Awakening</span>
+      <span>© {new Date().getFullYear()} Pandora&apos;s Awakening</span>
       <span>&nbsp;|&nbsp;</span>
       <a href='/privacy-policy' className="underline hover:text-pandoras-orange">Privacy Policy</a>
       <span>&nbsp;|&nbsp;</span>
