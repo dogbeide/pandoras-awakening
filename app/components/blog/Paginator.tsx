63750556b95a1d0af6626a090000b171ae02fd06
David Ogbeide <boyowadavid@gmail.com>
2024-10-23 22:02:45 -0400

    add previous button for blog pagination

--- app/components/blog/Paginator.tsx
+++ app/components/blog/Paginator.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
   export default function Paginator({ startCursor, endCursor, hasPreviousPage, hasNextPage} : {
     startCursor?: string,
@@ -7,14 +10,16 @@ import Link from "next/link"
     hasNextPage?: boolean
   }) {
 
+    const router = useRouter();
+
     return (
       <div className="w-full place-items-center my-2">
         <div className="grid grid-cols-2 gap-1 text-center">
-          {/* <Link href={`/blog/${startCursor}`} aria-disabled={!hasPreviousPage} className={`p-1 text-lg max-md:text-base max-sm:text-sm border border-solid 
+          <button onClick={router.back} aria-disabled={!hasPreviousPage} className={`p-1 text-lg max-md:text-base max-sm:text-sm border border-solid 
             ${hasPreviousPage ? 
               'cursor-pointer text-pandoras-orange hover:text-pandoras-lavender border-slate-300 ' : 
               'cursor-default pointer-events-none text-slate-300 border-slate-200 '}`}>&larr;...Previous
-          </Link> */}
+          </button>
           
           <Link href={`/blog/${endCursor}`} aria-disabled={!hasNextPage} className={`p-1 text-lg max-md:text-base max-sm:text-sm border border-solid 
             ${hasNextPage ? 
