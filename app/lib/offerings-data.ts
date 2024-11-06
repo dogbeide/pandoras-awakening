e1269f235b49becb3ae71d6dc402760d3b74ded4
David Ogbeide <boyowadavid@gmail.com>
2024-11-06 16:52:05 -0500

    reorder offerings-data and update offerings

--- app/lib/offerings-data.ts
+++ app/lib/offerings-data.ts
@@ -1,12 +1,5 @@
 export const offeringsPreview = [
   {
-    title: 'Sex Therapy',
-    desc: `If you are struggling with issues of sexual intimacy or desire, 
-          we can help you chart a path forward in your sex life.`,
-    href: '/offerings/sex-therapy',
-    imgUrl: '/Renee-Burwell-Sex-Therapy-thumbnail.webp'
-  },
-  {
     title: 'Couples Therapy',
     desc: `If your relationship struggles from ongoing communication issues or conflict, 
           we can help you find common ground.`,
@@ -14,11 +7,17 @@ export const offeringsPreview = [
     imgUrl: '/Renee-Burwell-couples-therapy-thumbnail-min.webp'
   },
   {
-    title: 'Psychedelic-Assisted Therapy',
-    desc: `A therapeutic approach that incorporates the use of psychedelic substances 
-          under controlled conditions, guided by trained facilitators, to enhance the therapeutic process.`,
-    href: '/offerings/psychedelic-assisted-therapy',
-    imgUrl: '/pexels-mccutcheon-1212408_150x150.jpg'
+    title: 'Healthy Families',
+    desc: ``,
+    href: '/offerings/healthy-families',
+    imgUrl: '/meal-table_150x150.webp'
+  },
+  {
+    title: 'Sex Therapy',
+    desc: `If you are struggling with issues of sexual intimacy or desire, 
+          we can help you chart a path forward in your sex life.`,
+    href: '/offerings/sex-therapy',
+    imgUrl: '/Renee-Burwell-Sex-Therapy-thumbnail.webp'
   },
   {
     title: 'Therapy for People of Color',
@@ -26,6 +25,13 @@ export const offeringsPreview = [
           we specialize in therapy for people of color.`,
     href: '/offerings/therapy-for-people-of-color',
     imgUrl: '/Pandoras-Awakening-Woman-therapyforpeopleofcolor-thumbnail.webp'
+  },
+  {
+    title: 'Intensive Therapy',
+    desc: `Pandora's Awakening is now offering intensive Couples and/or EMDR therapy packages. 
+          Our intensive packages include 3, 6, 9 or twelve hours of therapy over a one or two-day time period.`,
+    href: '/offerings/intensive-therapy',
+    imgUrl: '/intensive-therapy.webp'
   },
   {
     title: 'Trauma Therapy',
@@ -42,41 +48,46 @@ export const offeringsPreview = [
     imgUrl: '/cosmic.webp'
   },
   {
-    title: 'Concierge Counseling',
+    title: 'Psychedelic-Assisted Therapy',
+    desc: `A therapeutic approach that incorporates the use of psychedelic substances 
+          under controlled conditions, guided by trained facilitators, to enhance the therapeutic process.`,
+    href: '/offerings/psychedelic-assisted-therapy',
+    imgUrl: '/pexels-mccutcheon-1212408_150x150.jpg'
+  },
+  {
+    title: 'Concierge Counseling and Coaching',
     desc: `Pandora's Awakening offers concierge counseling as a means to create additional confidentiality 
           for those needing a therapist that meets you where you are.`,
     href: '/offerings/concierge-counseling',
     imgUrl: '/concierge_150x150.webp'
   },
   {
-    title: 'Intensive Therapy',
-    desc: `Pandora's Awakening is now offering intensive Couples and/or EMDR therapy packages. 
-          Our intensive packages include 3, 6, 9 or twelve hours of therapy over a one or two-day time period.`,
-    href: '/offerings/intensive-therapy',
-    imgUrl: '/intensive-therapy.webp'
+    title: 'Workshops and Events and Speaking Engagements',
+    desc: ``,
+    href: '/offerings/events',
+    imgUrl: '/events_150x150.png'
   },
-  {
-    title: 'MusiCares',
-    desc: `Pandora's Awakening is in collaboration with MusiCares 
-    to offer individuals and couples in the music industry mental and sexual health services. `,
-    href: '/offerings/musicares',
-    imgUrl: '/musicares_150x150.webp'
-  },
-  {
-    title: 'Kundalini Yoga',
-    desc: `Always remember to honor your body and participate to the degree that you are comfortable. 
-          Kundalini Yoga is perfect for beginners, so join us even if you have never done any yoga.`,
-    href: '/offerings/kundalini-yoga',
-    imgUrl: '/Renee-kundalini-yoga_150x150.webp'
-  },
-  {
-    title: 'Media/Speaking Engagements',
-    desc: `Renee is available for speaking engagements and events. 
-           Please schedule an appointment for more information.`,
-    href: '/offerings/media-speaking-engagements',
-    imgUrl: '/Renee-speaking-engagements_150x150.webp'
-  },
-  
+  // {
+  //   title: 'MusiCares',
+  //   desc: `Pandora's Awakening is in collaboration with MusiCares 
+  //   to offer individuals and couples in the music industry mental and sexual health services. `,
+  //   href: '/offerings/musicares',
+  //   imgUrl: '/musicares_150x150.webp'
+  // },
+  // {
+  //   title: 'Kundalini Yoga',
+  //   desc: `Always remember to honor your body and participate to the degree that you are comfortable. 
+  //         Kundalini Yoga is perfect for beginners, so join us even if you have never done any yoga.`,
+  //   href: '/offerings/kundalini-yoga',
+  //   imgUrl: '/Renee-kundalini-yoga_150x150.webp'
+  // },
+  // {
+  //   title: 'Media/Speaking Engagements',
+  //   desc: `Renee is available for speaking engagements and events. 
+  //          Please schedule an appointment for more information.`,
+  //   href: '/offerings/media-speaking-engagements',
+  //   imgUrl: '/Renee-speaking-engagements_150x150.webp'
+  // },
 ];
 
 export const offeringsDetail = {
