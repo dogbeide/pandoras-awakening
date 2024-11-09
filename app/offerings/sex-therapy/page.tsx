64ef3e0e67a151a4fb9de03ad1ff8a3a88d51d3c
David Ogbeide <boyowadavid@gmail.com>
2024-11-09 13:27:05 -0500

    add /offerings/sex-therapy

---
+++ app/offerings/sex-therapy/page.tsx
@@ -0,0 +1,321 @@
+import BtnCTA from "@/app/components/buttons/btn-cta";
+import Section from "@/app/components/general/Section";
+import SubTitle from "@/app/components/general/SubTitle";
+import Title from "@/app/components/text/Title";
+import { scheduleUrl } from "@/app/lib/links-data";
+
+export default function Page() {
+
+  return (
+    <Section classNames="bg-pandoras-purple [&>SubTitle]:text-pandoras-orange [&>*]:mb-5 
+      leading-7 [&>p]:text-white [&>h3]:text-white [&>h3]:text-xl"
+    >
+      <Title classNames="text-white">
+        Sex Therapy
+      </Title>{" "}
+      <SubTitle>
+        <span style={{ fontWeight: 400 }}>
+          Have You Come Up Against Obstacles To A Fulfilling Sex Life?
+        </span>
+      </SubTitle>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          Do you have questions about your sexual identity? Are you looking to
+          explore new erotic or intimate territory with your significant other? Or
+          do you struggle with challenges related to your sexual health?
+        </span>
+        <span style={{ fontWeight: 400 }}>
+          <img
+            // decoding="async"
+            // fetchpriority="high"
+            alt=""
+            width={200}
+            height={300}
+            data-srcset="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-2-min-200x300.jpg?lossy=1&strip=1&webp=1 200w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-2-min-125x187.jpg?lossy=1&strip=1&webp=1 125w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-2-min.jpg?lossy=1&strip=1&webp=1 400w"
+            data-src="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-2-min-200x300.jpg?lossy=1&strip=1&webp=1"
+            data-sizes="(max-width: 200px) 100vw, 200px"
+            className="size-medium wp-image-16476 alignleft lazyloaded"
+            src="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-2-min-200x300.jpg?lossy=1&strip=1&webp=1"
+            sizes="(max-width: 200px) 100vw, 200px"
+            srcSet="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-2-min-200x300.jpg?lossy=1&strip=1&webp=1 200w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-2-min-125x187.jpg?lossy=1&strip=1&webp=1 125w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-2-min.jpg?lossy=1&strip=1&webp=1 400w"
+          />
+        </span>
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          You may be experiencing doubts, questions, or limitations when it comes
+          to your sexual identity. Perhaps you and your partner have differing
+          wants and needs in the bedroom. Or maybe you’re curious about opening
+          your relationship up and becoming polyamorous.&nbsp;
+        </span>
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          It’s possible that your sexual functioning has changed over time, and
+          you are now struggling with aspects of anxiety and grief when it comes
+          to performing. Maybe a medical issue has caused a decrease in your
+          desire, or perhaps you struggle with vaginismus (vaginal pain during
+          sexual intercourse) or erectile dysfunction. If this is the case, you
+          may become stressed or worried about the idea of being intimate,
+          disappointed that sex is no longer as enjoyable or comfortable as it
+          once was.&nbsp;
+        </span>
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          When drive and desire between partners differ, it can create tension in
+          the relationship. It’s entirely possible that you and your partner no
+          longer experience the same passion and satisfaction as you once did. And
+          as a result, you may be looking for support in navigating a path
+          forward.&nbsp;
+        </span>
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          At Pandora’s Awakening, you can work with a sex therapist who will honor
+          your unique needs and desires within the relationship. And whether you
+          identify as gay or straight, monogamous or poly, trans or cis, white or
+          Person of Color (POC), therapy can help you identify and reach your
+          goals for your sex life.&nbsp;
+        </span>
+      </p>
+      <SubTitle>
+        <span style={{ fontWeight: 400 }}>
+          Our Culture Has Made Sex A Complicated Issue
+        </span>
+      </SubTitle>
+      <p>
+        <span style={{ fontWeight: 400 }}>A</span>
+        <span style={{ fontWeight: 400 }}>
+          lthough many of us understand that we are sexual beings, many are taught
+          to neglect and ignore versus
+        </span>
+        nurture this part of our humanity. We aren’t given a ton of vocabulary or
+        direction to express or understand how our sexuality changes over time.
+        Gender and sexuality exist on a spectrum after all, and what drives our
+        passion or desire at one point may alter greatly throughout our sexual
+        journey.
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          It doesn’t help, either, that the media and our culture have adopted a
+          complex and sometimes shameful view of sexuality. For instance, images
+          of women’s cleavage as a point of desire are common and accepted, but a
+          mother who is breastfeeding in public is balked at.&nbsp;
+        </span>
+      </p>
+      <p>
+        <img
+          // decoding="async"
+          alt=""
+          width={200}
+          height={300}
+          data-srcset="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-1-min-200x300.jpg?lossy=1&strip=1&webp=1 200w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-1-min-125x187.jpg?lossy=1&strip=1&webp=1 125w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-1-min.jpg?lossy=1&strip=1&webp=1 400w"
+          data-src="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-1-min-200x300.jpg?lossy=1&strip=1&webp=1"
+          data-sizes="(max-width: 200px) 100vw, 200px"
+          className="size-medium wp-image-16475 alignright lazyloaded"
+          src="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-1-min-200x300.jpg?lossy=1&strip=1&webp=1"
+          sizes="(max-width: 200px) 100vw, 200px"
+          srcSet="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-1-min-200x300.jpg?lossy=1&strip=1&webp=1 200w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-1-min-125x187.jpg?lossy=1&strip=1&webp=1 125w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-1-min.jpg?lossy=1&strip=1&webp=1 400w"
+        />
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          Moreover, many of us grew up in strict or religious environments where
+          our bodies and sex lives were surveilled, censored, or shamed for one
+          reason or another. As a result, we may feel uncomfortable with,
+          embarrassed by, or confused about our sexual needs, further complicating
+          the relationship we have with ourselves and our partners.&nbsp;
+        </span>
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          And for those of us who are people of color, society has not given us
+          many opportunities to fully appreciate or understand our bodies. Sex is
+          often portrayed through a lens of whiteness, not valuing our bodies in a
+          way that allows us to fully honor our desires and sexual function.&nbsp;
+        </span>
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          But at Pandora’s Awakening, our sex therapists will work with you to
+          decolonize the negative scripts that you have been subjected to when it
+          comes to your understanding of yourself and your body. Sex
+          therapy—including for people of color in particular—can help you to
+          navigate and resolve issues of sexual intimacy.&nbsp;
+        </span>
+      </p>
+      <SubTitle>
+        <span style={{ fontWeight: 400 }}>
+          Sex Therapy Gives You A Chance To Better Understand Yourself And Your
+          Body
+        </span>
+      </SubTitle>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          Like most of us, you may have developed negative associations with your
+          body and sexuality throughout your adulthood. Yet sex therapy can
+          normalize your experience and give you the necessary tools for
+          overcoming obstacles to your passion, intimacy, and sexual fulfillment.
+          And if you struggle with medical issues, including vaginismus or
+          erectile dysfunction, counseling can help you to better understand what
+          you need in order to feel satisfied on your sexual journey.&nbsp;&nbsp;
+        </span>
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          At Pandora’s Awakening, we see both individuals and couples for sex
+          counseling. Beginning with a brief consultation, we will work to
+          determine the presenting issues in your sex life and match you with the
+          therapist that is best suited to help you overcome challenges. As we go
+          through the intake process (spanning over a few sessions), your
+          therapist will be able to develop a treatment plan geared toward your
+          needs as an individual or couple.
+        </span>
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          Throughout your time in sex therapy, you will be invited to delve into
+          your history as a sexual being. This includes examining the possible
+          trauma and grief that have impacted your relationship with yourself and
+          your body. And whether you’re participating as an individual or a
+          couple, counseling provides a great opportunity to learn more about how
+          your and your partner’s desires were shaped and the kinds of challenges
+          that you both may face when it comes to issues of sexual intimacy.&nbsp;
+        </span>
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          Moreover, your therapist will work with you to help you unlearn the
+          negative or counterproductive education you’ve received when it comes to
+          understanding your body. In doing so, therapy can help you develop a
+          critical awareness of the emotional aspects of intimacy, as well as the
+          tools for processing the relational, psychosexual, and mental elements
+          of your sexuality.&nbsp;
+        </span>
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          Our approach to sex therapy often involves mindfulness techniques and
+          tools that revolve around a deeper awareness of the sensations occurring
+          in your body. And while sex therapy sessions are customized to meet the
+          needs of each individual or couple, we are likely to draw from
+          frameworks developed by such renowned sex and relationship therapists as
+          Masters and Johnson, The Kinsey Institute, Helen Kaplan, Esther Perel,
+          and Emily Nagoski. These experts have pioneered the field of sex therapy
+          and offer a new, nonbinary, and radically accepting way of exploring
+          sex, intimacy, and the body.&nbsp;
+        </span>
+      </p>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          With the right support, it’s possible to have a satisfying and
+          pleasurable sex life. No matter if your intimacy issues stem from
+          physical discomfort, emotional distress, or cultural limitations, with
+          therapy at Pandora’s Awakening, you can achieve a more satisfying and
+          intimate sex life.&nbsp;
+        </span>
+      </p>
+      <h3>
+        <i>
+          <span style={{ fontWeight: 400 }}>
+            Perhaps you’re interested in counseling for your sex life, but you
+            have some questions…&nbsp;
+          </span>
+        </i>
+      </h3>
+      <h3>
+        <i>
+          <span style={{ fontWeight: 400 }}>
+            Is there nudity or any form of obscenity in sex therapy?
+          </span>
+        </i>
+      </h3>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          Sex therapy is meant to be a profound mental and emotional exploration
+          of your sexuality. As such, counseling is not a workshop or instruction
+          in the physical act of sex, but rather an opportunity to overcome the
+          various obstacles that have caused you to feel unsatisfied in the
+          bedroom. We will certainly make space to discuss specific organs, acts,
+          and physical sensations, but there is no nudity—nor is sex therapy
+          intended to be obscene, uncomfortable, or profane.
+        </span>
+      </p>
+      <p>
+        <img
+          // decoding="async"
+          alt=""
+          width={200}
+          height={300}
+          data-srcset="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-3-min-200x300.jpg?lossy=1&strip=1&webp=1 200w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-3-min-125x187.jpg?lossy=1&strip=1&webp=1 125w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-3-min.jpg?lossy=1&strip=1&webp=1 400w"
+          data-src="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-3-min-200x300.jpg?lossy=1&strip=1&webp=1"
+          data-sizes="(max-width: 200px) 100vw, 200px"
+          className="size-medium wp-image-16477 alignleft lazyloaded"
+          src="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-3-min-200x300.jpg?lossy=1&strip=1&webp=1"
+          sizes="(max-width: 200px) 100vw, 200px"
+          srcSet="https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-3-min-200x300.jpg?lossy=1&strip=1&webp=1 200w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-3-min-125x187.jpg?lossy=1&strip=1&webp=1 125w, https://b1122150.smushcdn.com/1122150/wp-content/uploads/2021/08/Renee-Burwell-Sex-Therapy-3-min.jpg?lossy=1&strip=1&webp=1 400w"
+        />
+      </p>
+      <h3>
+        <i>
+          <span style={{ fontWeight: 400 }}>
+            Do you offer sex counseling for couples?&nbsp;
+          </span>
+        </i>
+      </h3>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          Absolutely. In fact, if you are partnered, we would{" "}
+        </span>
+        <i>
+          <span style={{ fontWeight: 400 }}>prefer</span>
+        </i>
+        <span style={{ fontWeight: 400 }}>
+          {" "}
+          that you attend sex therapy as a couple or group (for those in
+          polyamorous relationships). That way, everyone can benefit from the
+          perspective of a sex therapist as you learn more about your sexual needs
+          both as individuals and as a couple/group.&nbsp;
+        </span>
+      </p>
+      <h3>
+        <i>
+          <span style={{ fontWeight: 400 }}>
+            Is sex therapy a good place to explore BDSM or other kinks?
+          </span>
+        </i>
+      </h3>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          If you are curious about introducing something new and exciting into
+          your sex life, therapy is a great chance to explore that possibility.
+          All of our therapists are professional and sex-positive clinicians who
+          can help you better understand your desires, including desires within
+          the context of a kink or BDSM. We welcome all sexual appetites and
+          varieties!&nbsp;
+        </span>
+      </p>
+      <SubTitle>
+        <span style={{ fontWeight: 400 }}>
+          You Can Develop A Sense Of Ownership Over Your Body And Desires
+        </span>
+      </SubTitle>
+      <p>
+        <span style={{ fontWeight: 400 }}>
+          If you are struggling with issues of sexual intimacy or desire, therapy
+          at Pandora’s Awakening can help you chart a path forward in your sex
+          life. To schedule a 15-minute consultation or to learn more about how we
+          can help,{" "}
+        </span>
+        <a href={scheduleUrl} className="underline text-pandoras-orange hover:text-pandoras-lavender" target="_blank">
+          <span style={{ fontWeight: 400 }}>please fill out this form</span>
+        </a>
+        <span style={{ fontWeight: 400 }}>.</span>
+      </p>
+      <BtnCTA ariaLabel="Schedule Appointment" classNames="px-2 mt-4 text-white" fontSize="22px" href={scheduleUrl} target="_blank">
+        SCHEDULE APPOINTMENT
+      </BtnCTA>
+    </Section>
+  )
+}
