0472220cc0c27586027a45597e24b5e3f053aaa3
David Ogbeide <boyowadavid@gmail.com>
2024-11-17 15:35:19 -0500

    implement /offerings/psychedelic-assisted-therapy page

--- app/offerings/psychedelic-assisted-therapy/page.tsx
+++ app/offerings/psychedelic-assisted-therapy/page.tsx
@@ -20,68 +20,95 @@ export default function Page() {
     >
       <Title classNames="text-white">Psychedelic-Assisted Therapy</Title>
 
-      <SubTitle>
-        <span style={{ fontWeight: 400 }}>
-          Thriving Families is a subset of Pandora’s Awakening.
-        </span>
-      </SubTitle>
+      <Image
+        src='/pexels-mccutcheon-1212408_150x150.jpg'
+        alt='psychedelic therapy display img'
+        width={150}
+        height={150}
+      />
 
       <p className="font-bold">
-        Thriving Families focuses on couples and adults navigating transitions with having families, parenting, and caregiving family members and partners.   We believe that strong families are the foundation of a thriving community. Our mission is to support families in navigating challenges, enhancing communication, and fostering resilience.
+        Psychedelic-assisted therapy (PAT) is a therapeutic approach that incorporates the use
+        of psychedelic substances, such as ketamine, psilocybin, MDMA, LSD, bufo and
+        ayahuasca, to enhance the therapeutic process. These substances are used under
+        controlled conditions and guided by trained facilitators to help patients achieve greater
+        self-awareness, emotional healing, and psychological insights.
       </p>
 
-      <SubTitle>Our Approach</SubTitle>
 
-      <p className="">We provide a safe, nurturing environment where every family member can express their feelings and experiences. Our licensed therapists utilize evidence-based techniques to help families:</p>
-      <ul className="list-disc list-inside">
-        <li>Improve communication skills</li>
-        <li>Resolve conflicts constructively</li>
-        <li>Strengthen emotional bonds</li>
-        <li>Navigate life transitions together</li>
-        <li>Build coping strategies for stress and adversity</li>
-      </ul>
+      <p className="">
+        At Pandora’s Awakening, we do not facilitate medicine journeys in office, but can assist
+        in the preparation and integration process of your psychedelic experience. We can
+        assist with accessing providers upon request.
+      </p>
 
-      <SubTitle>Services Offered</SubTitle>
+      <p>
+        If you are considering a journey or have taken a journey and would like to go deeper in
+        integrating your experience, we are here to help.
+      </p>
+
+      <SubTitle>Preparation</SubTitle>
+
+      <p>
+        At Pandora’s Awakening, we can assist in educating on different medicines, setting
+        clear intentions for medicine work, vetting providers, and screening for suitability of
+        psychedelic-assisted therapy.
+      </p>
+
+      <SubTitle>Integration</SubTitle>
+      
+      <p className="font-bold">Integration is what helps make sense of the psychedelic journey.</p>
+      <p>
+        At Pandora&apos;s
+        Awakening, we recommend a 12-week integration strategy after medicine journeys.
+        This is to help with processing the experience and integrating lessons learned into
+        everyday life. We will also work on creating habits and accountability methods to fully
+        integrate the psychedelic experience.
+      </p>
+
+      <SubTitle>Potential Benefits of Psychedelic Assisted Therapy:</SubTitle>
 
       <ul className="[&>li]:mb-4">
         <li>
-          <span className="font-bold">Family Therapy: </span>
-          <span>Explore dynamics and improve relationships within the family unit.</span>
+          <span className="font-bold">Emotional Healing: </span>
+          <span>Patients often access repressed emotions and trauma, leading
+          to emotional release and healing.</span>
         </li>
         <li>
-          <span className="font-bold">Parenting Support: </span>
-          <span>Gain tools and strategies to enhance parenting skills and address behavioral challenges.</span>
+          <span className="font-bold">Insight and Self-Awareness: </span>
+          <span>The altered state of consciousness can provide deep
+          insights into personal issues, behaviors, and patterns.</span>
         </li>
         <li>
-          <span className="font-bold">Crisis Intervention: </span>
-          <span>Support families through difficult times with immediate strategies and resources.</span>
+          <span className="font-bold">Reduction in Symptoms: </span>
+          <span>Research shows promising results for conditions like
+          PTSD, suicidal ideations, depression, anxiety, and addiction.</span>
         </li>
         <li>
-          <span className="font-bold">Workshops: </span>
-          <span>Participate in interactive sessions focused on topics like effective communication, conflict resolution, and stress management.</span>
+          <span className="font-bold">Spiritual and Existential Insights: </span>
+          <span>Many patients report profound spiritual or
+          existential experiences that contribute to a sense of peace and well-being.</span>
         </li>
       </ul>
 
-      <SubTitle>Why Choose Us?</SubTitle>
+      {/* <SubTitle>Why Choose Us?</SubTitle>
 
       <ul className="[&>li]:mb-4">
         <li>
           <span className="font-bold">Experienced Therapists: </span>
-          <span>Our team has extensive training in family systems and therapy techniques.</span>
+          <span>Our team has extensive training in psychedelic therapy use.</span>
         </li>
         <li>
           <span className="font-bold">Personalized Care: </span>
-          <span>We tailor our approach to meet the unique needs of each family.</span>
+          <span>We tailor our approach to meet the unique needs of each individual.</span>
         </li>
         <li>
           <span className="font-bold">Confidential and Compassionate: </span>
           <span>We prioritize your privacy and approach every session with empathy and respect.</span>
         </li>
-      </ul>
+      </ul> */}
 
       <SubTitle>Take the First Step</SubTitle>
-
-      <p className="font-bold">Building a healthier family dynamic starts with a conversation. Contact us today to schedule a consultation and begin your journey toward stronger relationships.</p>
 
       <BtnCTA ariaLabel="Schedule Appointment" classNames="px-2 mt-4 text-white" fontSize="22px" href={scheduleUrl} target="_blank">
         SCHEDULE APPOINTMENT
