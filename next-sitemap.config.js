9090b5bbb11f92c9ae778616ffdc7c2c8f35b71d
David Ogbeide <boyowadavid@gmail.com>
2025-04-19 14:36:37 -0400

    update process.env.NEXT_PUBLIC_WORDPRESS_API_URL in next-sitemap config and public

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
+
+SITE_URL=process.env.NEXT_PUBLIC_WORDPRESS_API_URL || 'https://www.pandorasawakening.com';
+
 module.exports = {
-  siteUrl: 'https://www.pandorasawakening.com',
+  siteUrl: SITE_URL,
   generateRobotsTxt: true,
   sitemapSize: 7000,
 }
